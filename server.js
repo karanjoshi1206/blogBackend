676e665fccd7dff46c0f5dd65d33d94d9b1794e7
karanjoshi <joshikaran392@gmail.com>
2023-05-16 14:56:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const dotenv = require("dotenv")
+dotenv.config({ path: "./.env" })
 const app = require("./app")
 const mongoose = require("mongoose")
-const Blog = require("./models/blogModel")
 const port = process.env.PORT || 3004
-dotenv.config({ path: "./config.env" })
 
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
 
-mongoose.connect(DB).then(con => {
-    console.log("Connected to mongoDB \n")
-}).catch((err) => console.log("error is ", err))
 app.listen(port, () => {
     console.log("app is running on ", port)
 })
+mongoose.connect(DB).then(con => {
+    console.log("Connected to mongoDB \n")
+}).catch((err) => console.log("error is ", err))
