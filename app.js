1a31bc8c34de75a382ca457532525ac4b5d9979f
karanjoshi <joshikaran392@gmail.com>
2023-05-16 15:42:51 +0530

    cors added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const cors = require('cors')
+
 const app = express();
 const blogRouter = require("./routes/blogsRoute.js")
 const userRouter = require("./routes/userRoute.js")
 
+app.use(cors())
 app.use(express.json())
 
 
